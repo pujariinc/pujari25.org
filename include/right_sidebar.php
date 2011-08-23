344a83bdde05f300fd0c8479c1b176a540093cf8
Rupak Ganguly <rupakg@gmail.com>
2011-08-23 01:11:11 -0400

    Add Paypal button for DP tickets on right sidebar.

--- include/right_sidebar.php
+++ include/right_sidebar.php
@@ -1,8 +1,37 @@
 <div class="announcement">
   <h2>Announcements</h2>
-    <div class="text">
-      <p><strong>Pujari</strong> is participating in RBC 2011 in Franklin, TN on the week-end of June 24th, 2011 with two cultural programs. See details outside the RBC main auditorium.</p>
-      <p>25th year gala Durgatsav &prime;s ticket go on sale on Friday, June 24th.<br /><strong>Early bird special :</strong> $140/family </p>
+    <div>
+      <div class="text">
+        <strong>Durga Puja 2011 tickets on sale</strong>
+        We are happy to announce that the 3-day ticket package is on sale now, but not for long.
+        Grab the opportunity and buy your tickets online, right now!
+      </div>
+      <br />
+      <center>
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+          <input type="hidden" name="cmd" value="_s-xclick">
+          <input type="hidden" name="hosted_button_id" value="C9AVW7D7DYAJ6">
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+      </center>
+      <br />
+      <div class="text">
+        Brief overview of the Puja celebration and cultural events:
+        <ul>
+          <li><strong>Amontron Night, Fri., 30th Sept.</strong><br>
+            Registration, Reception, Networking, Pujo, Dinner, performance by Rathijit Bhattacharya
+          </li>
+          <li><strong>Kolkata Night, Sat., 1st Oct.</strong><br>
+            Pujo, Prasad, Lunch, Local Programs, Aroti,<br>
+            Dinner, performance by Tanushree Shankar Dance Company and Monomoy & Troupe
+          </li>
+          <li><strong>Bollywood Night, Sun., 2nd Oct.</strong><br>
+            Pujo, Lunch, Bisharjan,<br>
+            performance by Soumen Nandi & Pratibha Singh Baghel
+          </li>
+        </ul>
+      </div>
     </div>
     <!--<div class="read_more"><a href="#">read more &raquo;</a></div>-->
 </div>
