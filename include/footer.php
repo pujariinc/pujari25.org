d9120fdeb6367c4bca7dc89d225639e10c120083
Rupak Ganguly <rupakg@gmail.com>
2011-08-23 03:40:04 -0400

    Add a sponsor footer item. Add 2 new ads.

--- include/footer.php
+++ include/footer.php
@@ -1,37 +1,38 @@
  
-            <div class="bottom_content">
-            
-            	<div class="flickr_photo_stream">
-                	<h2><table width="100%" cellpadding="0" cellspacing="0"><tr><td align="left">FLICKR PHOTO STEAM</td><td align="right" class="read_more"><a href="http://www.flickr.com/photos/pujariinc/" target="_blank">view more &raquo;</a></td></tr></table></h2>
-
-                    <div id="wrap">
-                    	<div id="mycarousel" class="jcarousel-skin-ie7">
-                            <ul>
-                              <!-- The content will be dynamically loaded in here
-                              *** To change the photoset go to dynamic_flickr_feed.php page and change the set id                              
-                               -->
-                            </ul>
-                      	</div>
-					</div>
-
-
-                </div>
-                <div class="youtube_video">
-                	<h2><table width="100%" cellpadding="0" cellspacing="0"><tr><td align="left">YOUTUBE VIDEO CHANNEL</td><td align="right" class="read_more"><a href="#">view more &raquo;</a></td></tr></table></h2>
-                    <ul>
-                    	<li><a href="#"><img src="images/video_thumb/thumb_01.png" /></a></li>
-                        <li><a href="#"><img src="images/video_thumb/thumb_02.png" /></a></li>
-                    </ul>
-                </div>
-                
-            </div>
-        
-        
-        <div class="footer">
-        	<div class="footer_left"><a href="index.php">Home</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="mission.php">About Us</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="upcoming-events.php">Events</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="membership-benefits.php">Members</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="list-of-sponsors.php">Sponsors</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="anjali.php">Publication</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="important-links.php">Fast Track Info</a></div>
-            <div class="footer_right">&copy; 2011 Pujari Atlanta.  PO Box: 670842, Marietta, GA 30066-9998</div>
+    <div class="bottom_content">
+      <div class="flickr_photo_stream">
+        <h2><table width="100%" cellpadding="0" cellspacing="0"><tr><td align="left">FLICKR PHOTO STREAM</td><td align="right" class="read_more"><a href="http://www.flickr.com/photos/pujariinc/" target="_blank">view more &raquo;</a></td></tr></table></h2>
+        <div id="wrap">
+          <div id="mycarousel" class="jcarousel-skin-ie7">
+            <ul>
+              <!-- The content will be dynamically loaded in here
+              *** To change the photoset go to dynamic_flickr_feed.php page and change the set id
+               -->
+            </ul>
+          </div>
         </div>
+      </div>
+      <div class="youtube_video">
+        <h2><table width="100%" cellpadding="0" cellspacing="0"><tr><td align="left">YOUTUBE VIDEO CHANNEL</td><td align="right" class="read_more"><a href="#">view more &raquo;</a></td></tr></table></h2>
+        <ul>
+          <li><a href="#"><img src="images/video_thumb/thumb_01.png" /></a></li>
+          <li><a href="#"><img src="images/video_thumb/thumb_02.png" /></a></li>
+        </ul>
+      </div>
+      <div class="sponsors">
+        <h2><table width="100%" cellpadding="0" cellspacing="0"><tr><td align="left">OUR SPONSORS</td><td align="right" class="read_more"><!--<a href="#">view more &raquo;</a>--></td></tr></table></h2>
+        <ul>
+          <li><a href="ads/D-DISH-FC73D988_00001.png" target="_blank"><img src="ads/D-DISH-FC73D988_00001_thumb.png" /></a></li>
+          <li><a href="ads/CompleteInsurance.png" target="_blank"><img src="ads/CompleteInsurance_thumb.png" /></a></li>
+        </ul>
+      </div>
     </div>
+        
+    <div class="footer">
+      <div class="footer_left"><a href="index.php">Home</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="mission.php">About Us</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="upcoming-events.php">Events</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="membership-benefits.php">Members</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="list-of-sponsors.php">Sponsors</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="anjali.php">Publication</a> &nbsp; &nbsp; | &nbsp; &nbsp; <a href="important-links.php">Fast Track Info</a></div>
+      <div class="footer_right">&copy; 2011 Pujari Atlanta.  PO Box: 670842, Marietta, GA 30066-9998</div>
+    </div>
+  </div> <!-- end of div wrapper -->
 
 </center>
 
--- include/header.php
+++ include/header.php
@@ -36,58 +36,58 @@ function readmore(){
 <center>
 
 	<div id="wrapper">
-    	<div id="header">
-        	<div class="logo"><img src="images/logo.png" /></div>
-        </div>
-        <div class="menu">
-        	<ul>
-            	<li><a href="index.php">Home</a></li>
-                <li class="submenu">About Us
-                	<ul>
-                    	<li><a href="mission.php">Mission</a></li>
-                    	<li><a href="board-of-directors.php">Board of Directors</a></li>
-                    	<li><a href="executive-committee.php">Executive Committee</a></li>
-                    	<li><a href="history.php">History</a></li>
-                    	<li><a href="article-of-incorporation.php">Article of Incorporation</a></li>
-                    	<li class="last"><a href="by-laws.php">By Laws</a></li>
-                    </ul>
-                </li>
-                <li class="submenu">Events
-                	<ul>
-                    	<li><a href="upcoming-events.php">Upcoming Events</a></li>
-                    	<li class="last"><a href="event-archive.php">Event Archive</a></li>
-                    </ul>
-                </li>
-                <li class="submenu">Members
-                	<ul>
-                    	<li><a href="membership-benefits.php">Membership Benefits</a></li>
-                        <li class="last"><a href="become-a-member.php">Become A Member</a></li>
-                    </ul>
-                </li>
-                <li class="submenu">Sponsors
-                	<ul>
-                    	<li><a href="list-of-sponsors.php">List of Sponsors</a></li>
-                        <li class="last"><a href="pujari-yellow-pages.php">Pujari Yellow Pages</a></li>
-                    </ul>
-                </li>
-                <li class="submenu">Publication
-                	<ul>
-                    	<li class="last"><a href="anjali.php">Anjali</a></li>
-                    </ul>
-                </li>
-                <li class="submenu">Fast Track Info
-                	<ul>
-                    	<li><a href="important-links.php">Important Links</a></li>
-                    	<li><a href="amar-bangla.php">Amaar Bangla</a></li>
-                        <li class="last"><a href="go-global.php">Go Global</a></li>
-                    </ul>
-                </li>
+    <div id="header">
+      <div class="logo"><img src="images/logo.png" /></div>
+    </div>
+    <div class="menu">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li class="submenu">About Us
+          <ul>
+              <li><a href="mission.php">Mission</a></li>
+              <li><a href="board-of-directors.php">Board of Directors</a></li>
+              <li><a href="executive-committee.php">Executive Committee</a></li>
+              <li><a href="history.php">History</a></li>
+              <li><a href="article-of-incorporation.php">Article of Incorporation</a></li>
+              <li class="last"><a href="by-laws.php">By Laws</a></li>
             </ul>
-            
-            <div class="share">
-            	<a href="http://www.orkut.com/Community?cmm=87285505" target="_blank"><img src="images/icon_orkut.png" /></a>
-                <a href="http://twitter.com/pujariatl" target="_blank"><img src="images/icon_twitter.png" /></a>
-                <a href="http://www.facebook.com/group.php?gid=89581558856" target="_blank"><img src="images/icon_facebook.png" /></a>
-            </div>
-            <div class="clear"></div>
-        </div>
+        </li>
+        <li class="submenu">Events
+          <ul>
+              <li><a href="upcoming-events.php">Upcoming Events</a></li>
+              <li class="last"><a href="event-archive.php">Event Archive</a></li>
+            </ul>
+        </li>
+        <li class="submenu">Members
+          <ul>
+              <li><a href="membership-benefits.php">Membership Benefits</a></li>
+                <li class="last"><a href="become-a-member.php">Become A Member</a></li>
+            </ul>
+        </li>
+        <li class="submenu">Sponsors
+          <ul>
+              <li><a href="list-of-sponsors.php">List of Sponsors</a></li>
+                <li class="last"><a href="pujari-yellow-pages.php">Pujari Yellow Pages</a></li>
+            </ul>
+        </li>
+        <li class="submenu">Publication
+          <ul>
+              <li class="last"><a href="anjali.php">Anjali</a></li>
+            </ul>
+        </li>
+        <li class="submenu">Fast Track Info
+          <ul>
+              <li><a href="important-links.php">Important Links</a></li>
+              <li><a href="amar-bangla.php">Amaar Bangla</a></li>
+                <li class="last"><a href="go-global.php">Go Global</a></li>
+            </ul>
+        </li>
+      </ul>
+
+      <div class="share">
+        <a href="http://www.orkut.com/Community?cmm=87285505" target="_blank"><img src="images/icon_orkut.png" /></a>
+        <a href="http://twitter.com/pujariatl" target="_blank"><img src="images/icon_twitter.png" /></a>
+        <a href="http://www.facebook.com/group.php?gid=89581558856" target="_blank"><img src="images/icon_facebook.png" /></a>
+      </div>
+      <div class="clear"></div>
+    </div>
