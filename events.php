286379ed4ef3f1f70866eb2cb72f8f2f215f89e2
Rupak Ganguly <rupakg@gmail.com>
2011-08-30 02:57:37 -0400

    Add events page.

--- events.php
+++ events.php
@@ -1,4 +1,9 @@
-<?php 
+<?php
+include("include/top.php");
+?>
+<meta name="description" content="Pujari is a socio-cultural organization based in Atlanta, Georgia." />
+<meta name="keywords" content="Pujari, Cultural, Organization, Picnic, Cultural Programme, Baisakhi, Lakshmi Puja, Durga Puja, New Year Parties, Bangomela, Anjali, Publication, atlanta, georgia, bengali, durga puja, saraswati puja, laxmi puja, diwali, bengali club, association, cultural, bengali organization, puja, pooja, durga, saraswati, laxmi, lakshmi, rabindra sangeet, picnic, bengali community, community events, baisakhi, poila baishakhi, USA, new year's party, new year's eve, celebration, community participation, natok, dance drama, music kobita, drawing competition, kid's program, bengali adda">
+<?php
 include("include/header.php");  
 // ********* Add this part in every page **** //
 ?>
@@ -10,31 +15,121 @@ include("include/header.php");
             	<tr>
                 	<!-- Left Sidebar Start Here -->
                 	<td class="content_left">
-                    	<div class="recent_tweets">
-                        	<h2>Recent Tweets</h2>
-                            <div id="twitters">Tweets Loading...</div>
-                        </div>
-                        
-                        <div class="share">
-                        	<table width="100%" cellpadding="0" cellspacing="0">
-                            	<tr>
-                                	<td style="vertical-align:middle;" align="left"><h2>Share</h2></td>
-                                    <td align="right" style="padding-top:0px;"><img src="images/icon_orkut.png" alt="Orkut" /> &nbsp; <img src="images/icon_twitter.png" alt="Twitter" /> &nbsp; <img src="images/icon_facebook.png" alt="Facebook" /></td>
-                                </tr>
-                            </table>
-                        </div>
-                        
-                        <?php include("include/spread_love.php"); ?>
+                    <?php include("include/left_sidebar.php"); ?>
                     </td><!-- Left Sidebar End -->
                     
                     <!-- Content Area Start -->
                     <td class="content_mid">
-                    	<div class="header">
-                        	<h2>Events</h2>
-                            
-                            
-                            
+                      <div>
+                        <div class="header">
+                          <!-- Add homepage content after this -->
+                          <h2>Durga Puja 2011 - Cultural Program Timeline & Details</h2>
                         </div>
+                        <div class="event_item clear_both">
+                          <div class="date_bg float_left">
+                            <div class="month">Sep</div>
+                            <div class="date">30</div>
+                          </div>
+                          <div class="sub_content float_left">
+                            <h2>Amontron Night</h2>
+                            <h3>Friday, September 30, 2011</h3>
+                            <div class="text">
+                              <h4>Local Programs</h4>
+                              <ul>
+                                <li>7:00 pm to 8:15 pm  - Maa Durga Darshan & Reception</li>
+                                <li>8:15 pm to 9:20 pm  - Dinner, Adda & Networking</li>
+                                <li>9:20 pm to 9:30 pm  - Opening by Subhasree Nandy</li>
+                              </ul>
+                              <br/>
+                              <h4>Guest Artist Programs</h4>
+                              <ul>
+                                <li>9:30 pm to 11:00 pm - Rathijit Bhattacharya & Subhajit Ray (<strong>Ticketed</strong>)</li>
+                              </ul>
+                            </div>
+                            <br/>
+                            <div class="artist_info">
+                              <h4>Artist Profiles</h4>
+                              <div class="artist">
+                                <div class="photo">
+                                  <img src="images/artists/rathijit.png"/>
+                                </div>
+                                <div class="info">
+                                  <strong>Rathijit Bhattacharya</strong>
+                                  <br/>A regular singer and performer from Kolkata on Zee Bangla (SA RE GA MA PA) & ETV live shows. Music director of fight 1:1 (to be released soon)
+                                  <br/><br/><strong>With Subhajit Ray</strong>
+                                  <br/>An accomplished music organizer & TV show musician (Key Board Player & Piano Programmer) from Kolkata
+                                  <br/><br/><br/>
+                                </div>
+                                <div class="media">
+                                  <strong>Videos</strong><br/>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="event_item clear_both">
+                          <div class="date_bg float_left">
+                            <div class="month">Oct</div>
+                            <div class="date">01</div>
+                          </div>
+                          <div class="sub_content float_left">
+                            <h2>Kolkata Night</h2>
+                            <h3>Saturday, October 1st, 2011</h3>
+                            <div class="text">
+                              <h4>Local Programs</h4>
+                              <ul>
+                                <li>7:00 am to 1.00 pm - Puja, Prasad Bitaran, Bhog & Anjali</li>
+                                <li>1:00 pm to 2:45 pm - Lunch</li>
+                                <li>3:00 pm to 3:30 pm - Pujari 25th year Felicitation Ceremony <br/>(Direction & Host: Shyamoli Das)</li>
+                                <li>3:35 pm to 3:40 pm - Opening by Richa Sarkar</li>
+                                <li>3:45 pm to 4:15 pm - Ajob Desh Ajob Kahini - Children variety dance program <br/>(Choreography & Direction: Mayuri Ray)</li>
+                                <li>4:25 pm to 4:45 pm - Tagore’s Abhisaar - Dance program with young adults <br/>(Choreography & Direction: Bahni Nandi)</li>
+                                <li>4:55 pm to 5:45 pm - Sabbash Peto Pachu - Natok <br/>(Direction: Shyamoli Das)</li>
+                                <li>5:55 pm to 6:25 pm - Oasis Ek Din Ek Akash - Dance drama with youngsters & adults <br/>(Choreography & Direction: Indrani Kar)</li>
+                                <li>6:30 pm to 7:00 pm - Break & Arati</li>
+                              </ul>
+                              <br/>
+                              <h4>Guest Artist Programs</h4>
+                              <ul>
+                                <li>7:00 pm to 8:15 pm - Tanusree Shankar Dance Company (<strong>Ticketed</strong>)<br/>Ms Shankar and her troupe (six dancers) will
+                                  present contemporary dances like <br/>India Dance Creations (including Durga Vandana) & Chirantan. Introductions: Richa Samanta</li>
+                                <li>8:15 pm to 9:30 pm - Dinner</li>
+                                <li>9:30 pm to 11:00 pm - Manomay with his Band from Kolkata (<strong>Ticketed</strong>) <br/>Introductions: Richa Samanta</li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="event_item clear_both">
+                          <div class="date_bg float_left">
+                            <div class="month">Oct</div>
+                            <div class="date">2</div>
+                          </div>
+                          <div class="sub_content float_left">
+                            <h2>Bollywood Hangama Night</h2>
+                            <h3>Sunday, October 2nd, 2011</h3>
+                            <div class="float_right">
+                              <a href="images/banner/Bollywood_Night_Flyer.jpg" target="_blank"><img src="images/banner/Bollywood_Night_Flyer_thumb.jpg" alt="Pujari presents Bollywood Hangama Night 2011"></a>
+                            </div>
+                            <div class="text">
+                              <h4>Local Programs</h4>
+                              <ul>
+                                <li>9:00 am to 1.30 pm - Puja, Prasad Bitaran, Bhog & Anjali</li>
+                                <li>1:30 pm to 4:00 pm - Special Lunch & Adda</li>
+                                <li>4:00 pm to 4:30 pm - Dhunuchi Naach</li>
+                              </ul>
+                              <br/>
+                              <h4>Guest Artist Programs</h4>
+                              <ul>
+                                <li>4:50 pm to 5:00 pm - Opening & guest artist introductions by Neel Banerjee</li>
+                                <li>5:00 pm to 7:00 pm – Soumen & Pratibha (<strong>Ticketed</strong>)<br/>Zee TV’s SA RE GA MA PA 'Jodi No. 1'</li>
+                                <li>4:30 pm to 8:00 pm - Dinner/Snacks will be <strong>available for purchase</strong></li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+
+                        <!-- Add homepage content before this -->
+                      </div>
                     </td><!-- *********** Content Area End *************** -->
                     
                     
@@ -42,15 +137,7 @@ include("include/header.php");
                     
                 	<!-- ############# Right Sidebar Start Here ############## -->
                     <td class="content_right">
-                    	<div class="announcement">
-                        	<h2>Announcements</h2>
-                            <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                            <div class="read_more"><a href="#">read more &raquo;</a></div>
-                        </div>
-                        
-                        <div class="header">
-                        	<h2>Past Events</h2>
-                        </div>
+                      <?php include("include/right_sidebar.php"); ?>
                     </td><!-- ############# Right Sidebar End ############## -->
                     
                     
