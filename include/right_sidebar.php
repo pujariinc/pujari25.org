b52d9ec260dd83dd55f8a1c7d95f9ae3eb9819b8
Rupak Ganguly <rupakg@gmail.com>
2011-08-30 20:10:53 -0400

    Update verbiage for ticket package.

--- include/right_sidebar.php
+++ include/right_sidebar.php
@@ -3,7 +3,7 @@
     <div>
       <div class="text">
         <strong>Durga Puja 2011 tickets on sale</strong>
-        We are happy to announce that the 3-day ticket package is on sale now, but not for long.
+        We are happy to announce that the 3-day family ticket package is on sale now, but not for long.
         Grab the opportunity and buy your tickets online, right now!
       </div>
       <br />
@@ -17,20 +17,7 @@
       </center>
       <br />
       <div class="text">
-        Brief overview of the Puja celebration and cultural events:
-        <ul>
-          <li><strong>Amontron Night, Fri., 30th Sept.</strong><br>
-            Registration, Reception, Networking, Pujo, Dinner, performance by Rathijit Bhattacharya
-          </li>
-          <li><strong>Kolkata Night, Sat., 1st Oct.</strong><br>
-            Pujo, Prasad, Lunch, Local Programs, Aroti,<br>
-            Dinner, performance by Tanushree Shankar Dance Company and Monomoy & Troupe
-          </li>
-          <li><strong>Bollywood Night, Sun., 2nd Oct.</strong><br>
-            Pujo, Lunch, Bisharjan,<br>
-            performance by Soumen Nandi & Pratibha Singh Baghel
-          </li>
-        </ul>
+        <p><a href="events_DP2011.php">View full program times and details</a></p>
       </div>
     </div>
     <!--<div class="read_more"><a href="#">read more &raquo;</a></div>-->
