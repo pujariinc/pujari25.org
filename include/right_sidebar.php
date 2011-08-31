a8556e6773c8f4e2dc4fa02594b7d47d916a1e2e
Rupak Ganguly <rupakg@gmail.com>
2011-08-30 21:56:21 -0400

    Add posters to right sidebar.

--- include/right_sidebar.php
+++ include/right_sidebar.php
@@ -2,9 +2,13 @@
   <h2>Announcements</h2>
     <div>
       <div class="text">
-        <strong>Durga Puja 2011 tickets on sale</strong>
-        We are happy to announce that the 3-day family ticket package is on sale now, but not for long.
-        Grab the opportunity and buy your tickets online, right now!
+        <p><strong>Durga Puja 2011 tickets on sale</strong></p>
+        <div align="center">
+          <a href="images/banner/DP_2011_Flyer_web.jpg" target="_blank"><img src="images/banner/DP_2011_Flyer_thumb.jpg" /></a>
+          <br/><br/>
+        </div>
+        <p>We are happy to announce that the 3-day family ticket package is on sale now, but not for long.
+        Grab the opportunity and buy your tickets online, right now!</p>
       </div>
       <br />
       <center>
@@ -19,6 +23,15 @@
       <div class="text">
         <p><a href="events_DP2011.php">View full program times and details</a></p>
       </div>
+      <br/>
+      <div class="text">
+        <p><strong>Bollywood Hangama Night 2011 tickets on sale</strong></p>
+        <div align="center">
+          <a href="images/banner/Bollywood_Night_Flyer.jpg" target="_blank"><img src="images/banner/Bollywood_Night_Flyer_thumb.jpg" /></a>
+          <br/>
+        </div>
+      </div>
+      <br />
     </div>
     <!--<div class="read_more"><a href="#">read more &raquo;</a></div>-->
 </div>
