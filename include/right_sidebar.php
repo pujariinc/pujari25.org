b7d9d85b7091bbbc90ae70b1ee97871ccad64e80
Rupak Ganguly <rupakg@gmail.com>
2011-09-02 12:19:35 -0400

    Add more ticket packages on the Buy button.

--- include/right_sidebar.php
+++ include/right_sidebar.php
@@ -7,20 +7,32 @@
           <a href="images/banner/DP_2011_Flyer_web.jpg" target="_blank"><img src="images/banner/DP_2011_Flyer_thumb.jpg" /></a>
           <br/><br/>
         </div>
-        <p>We are happy to announce that the 3-day family ticket package is on sale now, but not for long.
+        <p>We are happy to announce that the 3-day family and 1-day individual ticket packages are for sale now, but not for long.
         Grab the opportunity and buy your tickets online, right now!</p>
       </div>
       <br />
       <center>
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-          <input type="hidden" name="cmd" value="_s-xclick">
-          <input type="hidden" name="hosted_button_id" value="C9AVW7D7DYAJ6">
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        <input type="hidden" name="cmd" value="_s-xclick">
+        <input type="hidden" name="hosted_button_id" value="C9AVW7D7DYAJ6">
+        <table>
+        <tr><td><input type="hidden" name="on0" value="Ticket Packages">Ticket Packages</td></tr><tr><td><select name="os0">
+          <option value="All 3 Days - Per Family">All 3 Days - Per Family $160.00</option>
+          <option value="All 3 Days - Per Adult">All 3 Days - Per Adult $80.00</option>
+          <option value="Saturday All Day - Per Adult">Saturday All Day - Per Adult $45.00</option>
+          <option value="Saturday Night - Per Adult">Saturday Night - Per Adult $35.00</option>
+          <option value="Sunday All Day - Per Adult">Sunday All Day - Per Adult $25.00</option>
+          <option value="Sunday Bollywood - Per Adult">Sunday Bollywood - Per Adult $15.00</option>
+          <option value="Sunday Bollywood - Per Child (10yrs+)">Sunday Bollywood - Per Child (10yrs+) $10.00</option>
+        </select> </td></tr>
+        </table>
+        <input type="hidden" name="currency_code" value="USD">
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
         </form>
       </center>
       <br />
-      <div class="text">
+      <div align="center" class="text">
         <p><a href="events_DP2011.php">View full program times and details</a></p>
       </div>
       <br/>
