08326971be2c5b48f5f09a0f806bde6d2ce80e82
Rupak Ganguly <rupakg@gmail.com>
2011-09-12 00:33:03 -0400

    Add bone marrow donor info. on right bar.

--- include/right_sidebar.php
+++ include/right_sidebar.php
@@ -1,51 +1,62 @@
 <div class="announcement">
   <h2>Announcements</h2>
-    <div>
-      <div class="text">
-        <p><strong>Durga Puja 2011 tickets on sale</strong></p>
-        <div align="center">
-          <a href="images/banner/DP_2011_Flyer_web.jpg" target="_blank"><img src="images/banner/DP_2011_Flyer_thumb.jpg" /></a>
-          <br/><br/>
-        </div>
-        <p>We are happy to announce that the 3-day family and 1-day individual ticket packages are for sale now, but not for long.
-        Grab the opportunity and buy your tickets online, right now!</p>
-      </div>
-      <br />
-      <center>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="C9AVW7D7DYAJ6">
-        <table>
-        <tr><td><input type="hidden" name="on0" value="Ticket Packages">Ticket Packages</td></tr><tr><td><select name="os0">
-          <option value="All 3 Days - Per Family">All 3 Days - Per Family $160.00</option>
-          <option value="All 3 Days - Per Adult">All 3 Days - Per Adult $80.00</option>
-          <option value="Saturday All Day - Per Adult">Saturday All Day - Per Adult $45.00</option>
-          <option value="Saturday Night - Per Adult">Saturday Night - Per Adult $35.00</option>
-          <option value="Sunday All Day - Per Adult">Sunday All Day - Per Adult $25.00</option>
-          <option value="Sunday Bollywood - Per Adult">Sunday Bollywood - Per Adult $15.00</option>
-          <option value="Sunday Bollywood - Per Child (10yrs+)">Sunday Bollywood - Per Child (10yrs+) $10.00</option>
-        </select> </td></tr>
-        </table>
-        <input type="hidden" name="currency_code" value="USD">
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-        </form>
-      </center>
-      <br />
-      <div align="center" class="text">
-        <p><a href="events_DP2011.php">View full program times and details</a></p>
-      </div>
-      <br/>
-      <div class="text">
-        <p><strong>Bollywood Hangama Night 2011 tickets on sale</strong></p>
-        <div align="center">
-          <a href="images/banner/Bollywood_Night_Flyer.jpg" target="_blank"><img src="images/banner/Bollywood_Night_Flyer_thumb.jpg" /></a>
-          <br/>
-        </div>
-      </div>
-      <br />
+  <div>
+    <div class="text">
+      <p><strong>Drive to join the Bone Marrow Donor Registry</strong></p>
+      <p>Location: Berkmar High School
+      <br/>Date/Time: Sun., Oct. 2nd, 2011 ‎1:00-5:00PM</p>
+      <p>We at Pujari, will be hosting at our upcoming Durga Puja, an extraordinary event,
+        which will give each of us, the opportunity to be such a "giver". We are planning
+        to host a drive to join the Bone Marrow Donor Registry.
+        If you are interested, but do not know what this will entail, please check the <a href="http://www.marrow.org/DONOR/When_You_re_Asked_to_Donate_fo/Donation_FAQs/index.html" target="_blank">donor</a> and <a href="http://www.marrow.org/JOIN/FAQs_about_Joining_the_Registry/index.html#how" target="_blank">FAQ</a> pages.</p>
     </div>
-    <!--<div class="read_more"><a href="#">read more &raquo;</a></div>-->
+  </div>
+  <div>
+    <div class="text">
+      <p><strong>Durga Puja 2011 tickets on sale</strong></p>
+      <div align="center">
+        <a href="images/banner/DP_2011_Flyer_web.jpg" target="_blank"><img src="images/banner/DP_2011_Flyer_thumb.jpg" /></a>
+        <br/><br/>
+      </div>
+      <p>We are happy to announce that the 3-day family and 1-day individual ticket packages are for sale now, but not for long.
+      Grab the opportunity and buy your tickets online, right now!</p>
+    </div>
+    <br />
+    <center>
+      <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="hosted_button_id" value="C9AVW7D7DYAJ6">
+      <table>
+      <tr><td><input type="hidden" name="on0" value="Ticket Packages">Ticket Packages</td></tr><tr><td><select name="os0">
+        <option value="All 3 Days - Per Family">All 3 Days - Per Family $160.00</option>
+        <option value="All 3 Days - Per Adult">All 3 Days - Per Adult $80.00</option>
+        <option value="Saturday All Day - Per Adult">Saturday All Day - Per Adult $45.00</option>
+        <option value="Saturday Night - Per Adult">Saturday Night - Per Adult $35.00</option>
+        <option value="Sunday All Day - Per Adult">Sunday All Day - Per Adult $25.00</option>
+        <option value="Sunday Bollywood - Per Adult">Sunday Bollywood - Per Adult $15.00</option>
+        <option value="Sunday Bollywood - Per Child (10yrs+)">Sunday Bollywood - Per Child (10yrs+) $10.00</option>
+      </select> </td></tr>
+      </table>
+      <input type="hidden" name="currency_code" value="USD">
+      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+      </form>
+    </center>
+    <br />
+    <div align="center" class="text">
+      <p><a href="events_DP2011.php">View full program times and details</a></p>
+    </div>
+    <br/>
+    <div class="text">
+      <p><strong>Bollywood Hangama Night 2011 tickets on sale</strong></p>
+      <div align="center">
+        <a href="images/banner/Bollywood_Night_Flyer.jpg" target="_blank"><img src="images/banner/Bollywood_Night_Flyer_thumb.jpg" /></a>
+        <br/>
+      </div>
+    </div>
+    <br />
+  </div>
+  <!--<div class="read_more"><a href="#">read more &raquo;</a></div>-->
 </div>
 
 <!--<div class="header">
