274b3ed97e34d4ed1ac0a754876ee5fcb4155322
Rupak Ganguly <rupakg@gmail.com>
2011-09-29 23:32:28 -0400

    Add Membership Buy button.

--- become-a-member.php
+++ become-a-member.php
@@ -22,18 +22,22 @@ include("include/header.php");
                     <td class="content_mid">
                     	<div class="header">
                         	<h2>Become a Member</h2>
-                            <div class="space_sml"></div>
-                            <div>
-                              <p> Pujari membership entitles you to a wide variety of benefits and special offers that range from building contacts, and skills that help   advance your career to free and highly discounted tickets for Pujari organized best-in-town events that save money, and much more....
-                                For details, visit <a href="membership-benefits.php">Membership Benefits</a></p>
-                              <br/>
-                              <h2>Membership Information and Form</h2>
-                              <p> Memberships are valid for ONE year, starting on the date payment is received.</p>
-                              <p> To become a Pujari member, please <a href="allpdf/membershipform.pdf" target="_blank">download</a> the membership form and print it. Then complete it and mail the form along with payments to the mailing address on the form (see instructions &amp; mailing address on the form).</p>
-                              <p><a href="allpdf/membershipform.pdf" target="_blank"><img src="images/icon_download.png"></a>&nbsp;&nbsp;<a href="allpdf/membershipform.pdf" target="_blank">Download the Membership Form</a> and become a member today and join the Pujari family!</p>
-                            </div>
-                            
-                        </div>
+                          <div class="space_sml"></div>
+                          <div>
+                            <p> Pujari membership entitles you to a wide variety of benefits and special offers that range from building contacts, and skills that help   advance your career to free and highly discounted tickets for Pujari organized best-in-town events that save money, and much more....
+                              For details, visit <a href="membership-benefits.php">Membership Benefits</a></p>
+                            <br/>
+                            <h2>Membership Information and Form</h2>
+                            <p> Memberships are valid for ONE year, starting on the date payment is received.</p>
+                            <p> To become a Pujari member, please <a href="allpdf/membershipform.pdf" target="_blank">download</a> the membership form and print it. Then complete it and mail the form along with payments to the mailing address on the form (see instructions &amp; mailing address on the form).</p>
+                            <p><a href="allpdf/membershipform.pdf" target="_blank"><img src="images/icon_download.png"></a>&nbsp;&nbsp;<a href="allpdf/membershipform.pdf" target="_blank">Download the Membership Form</a> and become a member today and join the Pujari family!</p>
+                          </div>
+                      </div>
+
+                      <!-- Start Membership order buy button -->
+                      <?php include("include/membership_order.php"); ?>
+                      <!-- End Membership order buy button -->
+
                     </td><!-- *********** Content Area End *************** -->
                     
                     
---
+++ include/membership_order.php
@@ -0,0 +1,32 @@
+<div class="header">
+  <h2>Membership Payment</h2>
+  <div class="membership_order">
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="hosted_button_id" value="ZE4CCUPAS63XC">
+      <table width="100%" cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="60%" valign="center">
+            <input type="hidden" name="on0" value=">"><strong>Membership Packages</strong>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="radio" name="os0" value="Family" checked="true"> Family $160.00<br/>
+            <input type="radio" name="os0" value="Student Family"> Student Family $140.00<br/>
+            <input type="radio" name="os0" value="Family with No kids or kids less than 7 yrs old"> Family with No kids or kids less than 7 yrs old $140.00<br/>
+            <input type="radio" name="os0" value="Individual"> Individual $70.00<br/>
+            <input type="radio" name="os0" value="Student Individual"> Student Individual $60.00<br/>
+          </td>
+          <td valign="center">
+            <br/>
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif"
+               border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </td>
+        </tr>
+      </table>
+    </form>
+  </div>
+</div>
