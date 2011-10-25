fbb6e43f7177268a8a0e311bd03f9a7ab7e1b825
Rupak Ganguly <rupakg@gmail.com>
2011-10-25 00:29:11 -0400

    Add guidelines to Anjali page. Add anjali logo and also add all the old anjali pdfs.

--- anjali.php
+++ anjali.php
@@ -12,103 +12,112 @@ include("include/header.php");
         
         <div id="content_container">
         	<table width="100%" cellpadding="0" cellspacing="0" border="0">
-            	<tr>
-                	<!-- Left Sidebar Start Here -->
-                	<td class="content_left">
-                    	<?php include("include/left_sidebar.php"); ?>
-                    </td><!-- Left Sidebar End -->
-                    
-                    <!-- Content Area Start -->
-                    <td class="content_mid">
-                    	<div class="header">
-                        	<h2>Anjali</h2>
-                            <div class="space_sml"></div>
-                            <div>
-                            	<p style="text-align: center;">
-	<img alt="" src="images/image_thumb/anjalilogo.png" style="width: 199px; height: 118px;"></p>
-<p>
-	<span class="style14">Anjali,</span> the collective endeavor of Pujari has been satisfying the literary cravings of the community since its inception. Pujari patrons look forward to the Sharodiya issues of Anjali in their full glory every year. Anjali brings out the colorful bouquets of collection contributed by the young the old members of Pujari as well as well-known writers from India, US and other countries.</p>
-<p>&nbsp;
-	</p>
-<div align="center">
-	<p class="link_heading">
-		<a class="style14" href="http://issuu.com/designoscope/docs/baisakhi_magazine" target="_blank">Baisakhi Anjali 2010</a></p>
+            <tr>
+              <!-- Left Sidebar Start Here -->
+              <td class="content_left">
+                <?php include("include/left_sidebar.php"); ?>
+              </td><!-- Left Sidebar End -->
 
-	<center>
-		<object style="width: 420px; height: 271px;"><param name="movie" value="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf?mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fcolor%2Flayout.xml&amp;backgroundColor=A4112B&amp;showFlipBtn=true&amp;documentId=100419124145-d16d17535c044ab3925b74ee755e6972&amp;docName=baisakhi_magazine&amp;username=designOscope&amp;loadingInfoText=Anjali%3A%20Baisakhi%20Issue%202010&amp;et=1271726432415&amp;er=32"><param name="allowfullscreen" value="true"><param name="menu" value="false"><embed allowfullscreen="true" flashvars="mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fcolor%2Flayout.xml&amp;backgroundColor=A4112B&amp;showFlipBtn=true&amp;documentId=100419124145-d16d17535c044ab3925b74ee755e6972&amp;docName=baisakhi_magazine&amp;username=designOscope&amp;loadingInfoText=Anjali%3A%20Baisakhi%20Issue%202010&amp;et=1271726432415&amp;er=32" menu="false" src="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf" style="width: 420px; height: 271px;" type="application/x-shockwave-flash"></object></center>
-	<p class="link_heading">
-		<a class="style14" href="http://issuu.com/designoscope/docs/anjali_spuja2010" target="_blank">Saraswati Puja Anjali 2010</a></p>
-	<center>
-		<object style="width: 420px; height: 272px;"><param name="movie" value="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf?mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fcolor%2Flayout.xml&amp;backgroundColor=A4112B&amp;showFlipBtn=true&amp;documentId=100415171620-ef91353d4a644c1495ca507054738b9d&amp;docName=anjali_spuja2010&amp;username=designOscope&amp;loadingInfoText=Saraswati%20Pujati%20Issue%3A%20Anjali%202010&amp;et=1275448059587&amp;er=0"><param name="allowfullscreen" value="true"><param name="menu" value="false"><embed allowfullscreen="true" flashvars="mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fcolor%2Flayout.xml&amp;backgroundColor=A4112B&amp;showFlipBtn=true&amp;documentId=100415171620-ef91353d4a644c1495ca507054738b9d&amp;docName=anjali_spuja2010&amp;username=designOscope&amp;loadingInfoText=Saraswati%20Pujati%20Issue%3A%20Anjali%202010&amp;et=1275448059587&amp;er=0" menu="false" src="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf" style="width: 420px; height: 272px;" type="application/x-shockwave-flash"></object></center>
-	<table class="publication_tbl">
-		<tbody>
+              <!-- Content Area Start -->
+              <td class="content_mid">
+                <div class="header">
+                  <h2>Anjali</h2>
+                    <div class="space_sml"></div>
+                    <div>
+                      <p style="text-align: center;">
+                        <img alt="Anjali" src="images/anjalilogo.gif" style="width: 201px; height: 120px;">
+                      </p>
+                      <p>
+                        <span class="style14">Anjali,</span> the collective endeavor of Pujari has been satisfying the literary cravings
+                        of the community since its inception. Pujari patrons look forward to the Sharodiya issues of Anjali in their full
+                        glory every year. Anjali brings out the colorful bouquets of collection contributed by the young the old members
+                        of Pujari as well as well-known writers from India, US and other countries.
+                      </p>
+                      <p>
+                        If you are interested in submitting a literary piece, here are some <strong>simple guidelines</strong> to follow: <br/>
+                        <ul>
+                          <li>Articles from all ages on all subjects are welcome.</li>
+                          <li>Subject: Articles, Short story(up to 2 pages), Poems, Essays and Illustrations/drawings</li>
+                          <li>Language: Bangla & English</li>
+                          <li>Format: Word doc, Bangla font (gmail or bwd format) without any formatting.</li>
+                          <li><strong>Please do not send any copyrighted work</strong>. We do not publish articles that have been or may be published elsewhere, unless by prior agreement.</li>
+                        </ul>
+                        <br/>Send your document by email to <a href="mailto:editor@pujari.org">editor@pujari.org</a>, by the published submission deadline.
+                        <br/><br/>Pujari reserves the right to edit and modify
+                        any submitted literary piece to fit the context and format of the magazine. Pujari also reserves the right to choose which submitted literary
+                        pieces are finally published.
+                      </p>
 
-			<tr>
-				<td>
-					<p class="link_heading">
-						<a class="style14" href="/allpdf/Anjali2009_5.pdf" target="_blank">Sharodiya Anjali 2009</a></p>
-					<p class="style21">
-						<a href="/allpdf/Anjali2009_5.pdf" target="_blank"><img alt="Sharodiya Anjali 2009" src="images/image_thumb/Anjali2009.png" height="234" width="180"></a></p>
-				</td>
-				<td>
+                      <div align="center">
+                        <p class="link_heading">
+                          <a class="style14" href="http://issuu.com/designoscope/docs/baisakhi_magazine" target="_blank">Baisakhi Anjali 2010</a>
+                        </p>
+                        <center>
+                          <object style="width: 420px; height: 271px;"><param name="movie" value="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf?mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fcolor%2Flayout.xml&amp;backgroundColor=A4112B&amp;showFlipBtn=true&amp;documentId=100419124145-d16d17535c044ab3925b74ee755e6972&amp;docName=baisakhi_magazine&amp;username=designOscope&amp;loadingInfoText=Anjali%3A%20Baisakhi%20Issue%202010&amp;et=1271726432415&amp;er=32"><param name="allowfullscreen" value="true"><param name="menu" value="false"><embed allowfullscreen="true" flashvars="mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fcolor%2Flayout.xml&amp;backgroundColor=A4112B&amp;showFlipBtn=true&amp;documentId=100419124145-d16d17535c044ab3925b74ee755e6972&amp;docName=baisakhi_magazine&amp;username=designOscope&amp;loadingInfoText=Anjali%3A%20Baisakhi%20Issue%202010&amp;et=1271726432415&amp;er=32" menu="false" src="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf" style="width: 420px; height: 271px;" type="application/x-shockwave-flash"></object>
+                        </center>
+                        <p class="link_heading">
+                          <a class="style14" href="http://issuu.com/designoscope/docs/anjali_spuja2010" target="_blank">Saraswati Puja Anjali 2010</a>
+                        </p>
+                        <center>
+                          <object style="width: 420px; height: 272px;"><param name="movie" value="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf?mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fcolor%2Flayout.xml&amp;backgroundColor=A4112B&amp;showFlipBtn=true&amp;documentId=100415171620-ef91353d4a644c1495ca507054738b9d&amp;docName=anjali_spuja2010&amp;username=designOscope&amp;loadingInfoText=Saraswati%20Pujati%20Issue%3A%20Anjali%202010&amp;et=1275448059587&amp;er=0"><param name="allowfullscreen" value="true"><param name="menu" value="false"><embed allowfullscreen="true" flashvars="mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fcolor%2Flayout.xml&amp;backgroundColor=A4112B&amp;showFlipBtn=true&amp;documentId=100415171620-ef91353d4a644c1495ca507054738b9d&amp;docName=anjali_spuja2010&amp;username=designOscope&amp;loadingInfoText=Saraswati%20Pujati%20Issue%3A%20Anjali%202010&amp;et=1275448059587&amp;er=0" menu="false" src="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf" style="width: 420px; height: 272px;" type="application/x-shockwave-flash"></object>
+                        </center>
+                        <table class="publication_tbl">
+                          <tbody>
+                            <tr>
+                              <td>
+                                <p class="link_heading">
+                                  <a class="style14" href="allpdf/Anjali2009_5.pdf" target="_blank">Sharodiya Anjali 2009</a></p>
+                                <p class="style21">
+                                  <a href="allpdf/Anjali2009_5.pdf" target="_blank"><img alt="Sharodiya Anjali 2009" src="images/image_thumb/Anjali2009.png" height="234" width="180"></a></p>
+                              </td>
+                              <td>
 
-					<p class="link_heading">
-						<a class="style14" href="/allpdf/Anjali2008.pdf" target="_blank">Sharodiya Anjali 2008</a></p>
-					<p class="style21">
-						<a href="/allpdf/Anjali2008.pdf" target="_blank"><img alt="Sharodiya Anjali 2008" src="images/image_thumb/Anjali2008.jpg" height="234" width="180"></a></p>
-				</td>
-			</tr>
-			<tr>
-				<td>
+                                <p class="link_heading">
+                                  <a class="style14" href="allpdf/Anjali2008.pdf" target="_blank">Sharodiya Anjali 2008</a></p>
+                                <p class="style21">
+                                  <a href="allpdf/Anjali2008.pdf" target="_blank"><img alt="Sharodiya Anjali 2008" src="images/image_thumb/Anjali2008.jpg" height="234" width="180"></a></p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>
+                                <p class="link_heading">
+                                  <a class="link_heading" href="allpdf/sharodiya07.pdf" target="_blank">Sharodiya Anjali 2007</a></p>
+                                <p class="style21">
+                                  <a href="allpdf/sharodiya07.pdf" target="_blank"><img alt="Sharodiya Anjali 2007" src="images/image_thumb/anjali07.jpg" height="234" width="180"></a></p>
+                              </td>
+                              <td>
+                                <p class="link_heading">
+                                  <a class="link_heading" href="allpdf/Anjali06.pdf" target="_blank">Sharodiya Anjali 2006</a></p>
+                                <p class="style21">
+                                  <a href="allpdf/Anjali06.pdf" target="_blank"><img alt="Sharodiya Anjali 2006" src="images/image_thumb/anjali06.jpg" height="234" width="180"></a></p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>
+                                <p class="link_heading">
+                                  <a class="link_heading" href="allpdf/anjali5.pdf" target="_blank">Sharodiya Anjali 2005</a></p>
+                                <p class="style21">
+                                  <a href="allpdf/anjali5.pdf" target="_blank"><img alt="Sharodiya Anjali 2005" src="images/image_thumb/anjali05.jpg" height="234" width="180"></a></p>
+                              </td>
+                              <td>&nbsp;
+                                </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                </div>
+              </td><!-- *********** Content Area End *************** -->
 
-					<p class="link_heading">
-						<a class="link_heading" href="/allpdf/sharodiya07.pdf" target="_blank">Sharodiya Anjali 2007</a></p>
-					<p class="style21">
-						<a href="/allpdf/sharodiya07.pdf" target="_blank"><img alt="Sharodiya Anjali 2007" src="images/image_thumb/anjali07.jpg" height="234" width="180"></a></p>
-				</td>
-				<td>
-					<p class="link_heading">
-						<a class="link_heading" href="/allpdf/Anjali06.pdf" target="_blank">Sharodiya Anjali 2006</a></p>
+              <!-- ############# Right Sidebar Start Here ############## -->
+              <td class="content_right">
+                <?php include("include/right_sidebar.php"); ?>
+              </td><!-- ############# Right Sidebar End ############## -->
 
-					<p class="style21">
-						<a href="/allpdf/Anjali06.pdf" target="_blank"><img alt="Sharodiya Anjali 2006" src="images/image_thumb/anjali06.jpg" height="234" width="180"></a></p>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<p class="link_heading">
-						<a class="link_heading" href="/allpdf/anjali5.pdf" target="_blank">Sharodiya Anjali 2005</a></p>
-
-					<p class="style21">
-						<a href="/allpdf/anjali5.pdf" target="_blank"><img alt="Sharodiya Anjali 2005" src="images/image_thumb/anjali05.jpg" height="234" width="180"></a></p>
-				</td>
-				<td>&nbsp;
-					</td>
-			</tr>
-		</tbody>
-	</table>
-</div>
-
-                            </div>
-                            
-                        </div>
-                    </td><!-- *********** Content Area End *************** -->
-                    
-                    
-                    
-                    
-                	<!-- ############# Right Sidebar Start Here ############## -->
-                    <td class="content_right">
-                    	<?php include("include/right_sidebar.php"); ?>
-                    </td><!-- ############# Right Sidebar End ############## -->
-                    
-                    
-                </tr>
-            </table>
+            </tr>
+          </table>
         </div>
-        
-        
-        
+
 <?php
 include("include/footer.php");
 // ********* Add this part in every page **** //
