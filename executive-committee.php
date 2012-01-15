3e77dbc3698718bb8325392c42de5938a3e1678c
Rupak Ganguly <rupakg@gmail.com>
2012-01-15 00:02:02 -0500

    Update current 2012 executive committee lineup.

--- executive-committee.php
+++ executive-committee.php
@@ -1,8 +1,9 @@
 <?php
 include("include/top.php");
 ?>
-<meta name="description" content="">
-<meta name="keywords" content="">
+<meta name="description" content="Pujari is a socio-cultural organization based in Atlanta, Georgia." />
+<meta name="keywords" content="Pujari, Cultural, Organization, Picnic, Cultural Programme, Baisakhi, Lakshmi Puja, Durga Puja, New Year Parties, Bangomela, Anjali, Publication, atlanta, georgia, bengali, durga puja, saraswati puja, laxmi puja, diwali, bengali club, association, cultural, bengali organization, puja, pooja, durga, saraswati, laxmi, lakshmi, rabindra sangeet, picnic, bengali community, community events, baisakhi, poila baishakhi, USA, new year's party, new year's eve, celebration, community participation, natok, dance drama, music kobita, drawing competition, kid's program, bengali adda">
+<title>Pujari | Executive Committee</title>
 <?php
 include("include/header.php");  
 // ********* Add this part in every page **** //
@@ -30,13 +31,14 @@ include("include/header.php");
                                     <td valign="top"><table border="1" cellpadding="5" cellspacing="0" class="yloPgeTbl">
                                         <tbody>
                                           <tr>
-                                            <td colspan="3" valign="top" width="187"><h1 style="border-bottom:none; text-align:center;"> 2011 Executive Committee</h1></td>
+                                            <td colspan="3" valign="top" width="187">
+                                              <h1 style="border-bottom:none; text-align:center;"> 2012 Executive Committee</h1></td>
                                           </tr>
                                           <tr>
                                             <td valign="top" width="187"><p> <strong>President</strong></p></td>
-                                            <td valign="top" width="196"><p> P. K. Das</p></td>
-                                            <td valign="top" width="278"><p> <strong>Phone: 770-451-8587 </strong><br />
-                                              Email: <strong><a href="mailto:priya.k.das@gmail.com">priya.k.das@gmail.com</a></strong></p></td>
+                                            <td valign="top" width="196"><p>Sanjib Dutta</p></td>
+                                            <td valign="top" width="278"><p> <strong>Phone:  </strong><br />
+                                              Email: <strong><a href="mailto:samdatta2003@yahoo.com">samdatta2003@yahoo.com</a></strong></p></td>
                                           </tr>
                                           <tr>
                                             <td rowspan="2" valign="top" width="187"><p><strong>VP, Operations</strong></p></td>
@@ -45,68 +47,62 @@ include("include/header.php");
                                               Email: <strong><a href="mailto:ahazra@hotmail.com">ahazra@hotmail.com</a></strong></p></td>
                                           </tr>
                                           <tr>
-                                            <td valign="top" width="196"><p> Surojit Chatterjee</p></td>
-                                            <td valign="top" width="278"><p> Phone: 678-640-4151<br />
-                                              Email: <strong><a href="mailto:chsurajit@gmail.com">chsurajit@gmail.com</a></strong></p></td>
+                                            <td valign="top" width="196"><p> Prosenjit Dutta</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:duttap06@gmail.com">duttap06@gmail.com</a></strong></p></td>
                                           </tr>
                                           <tr>
                                             <td valign="top" width="187"><p> <strong>VP, Cultural</strong></p></td>
-                                            <td valign="top" width="196"><p> Raja Roy</p></td>
-                                            <td valign="top" width="278"><p> Phone: 770-557-1850&nbsp;&nbsp;<br />
-                                              Email: <strong><a href="mailto:debrituroy@yahoo.com">debrituroy@yahoo.com</a></strong></p></td>
+                                            <td valign="top" width="196"><p> Prabir Bhattacharyya</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:"></a></strong></p></td>
+                                          </tr>
+                                          <tr>
+                                            <td valign="top" width="187"><p><strong>Cultural Secretary</strong></p></td>
+                                            <td valign="top" width="196"><p> Rahul Ray</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:"></a></strong></p></td>
                                           </tr>
                                           <tr>
                                             <td valign="top" width="187"><p> <strong>VP, Finance</strong></p></td>
-                                            <td valign="top" width="196"><p> Swapan Mondal</p></td>
-                                            <td valign="top" width="278"><p> Phone: 404-642-0103&nbsp;&nbsp;<br />
-                                              Email: <strong><a href="mailto:swapanmondal@yahoo.com">swapanmondal@yahoo.com</a></strong></p></td>
-                                          </tr>
-                                          <tr>
-                                            <td valign="top" width="187"><p> <strong>Treasurer</strong></p></td>
                                             <td valign="top" width="196"><p> Vikram Das</p></td>
                                             <td valign="top" width="278"><p> Phone: 678-472-7016&nbsp;&nbsp;<br />
                                               Email: <strong><a href="mailto:dasvikram@yahoo.com">dasvikram@yahoo.com</a></strong></p></td>
                                           </tr>
                                           <tr>
                                             <td valign="top" width="187"><p> <strong>VP, Revenue Generation</strong></p></td>
-                                            <td valign="top" width="196"><p> Subhojit Roy</p></td>
-                                            <td valign="top" width="278"><p> Phone: 404-247-0181&nbsp;&nbsp;<br />
-                                              Email<strong>:</strong> <strong><a href="mailto:subhojitr@gmail.com">subhojitr@gmail.com</a></strong></p></td>
+                                            <td valign="top" width="196"><p> Debjyoti Roy (Raja)</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:"></a></strong></p></td>
                                           </tr>
                                           <tr>
                                             <td valign="top" width="187"><p> <strong>VP, Public Relations and Membership Drive</strong></p></td>
-                                            <td valign="top" width="196"><p> Pranesh Chowdhury</p></td>
-                                            <td valign="top" width="278"><p> Phone: 404-428-5628<br />
-                                              Email: <strong><a href="mailto:pranesh68@gmail.com">pranesh68@gmail.com</a></strong></p></td>
-                                          </tr>
-                                          <tr>
-                                            <td valign="top" width="187"><p><strong>Food &amp; Services Secretary</strong></p></td>
-                                            <td valign="top" width="196"><p> Niloofer Mondal</p></td>
-                                            <td valign="top" width="278"><p> Phone: 770-517-0783&nbsp;<br />
-                                              Email: <strong><a href="mailto:nmondal2000@yahoo.com">nmondal2000@yahoo.com</a></strong></p></td>
-                                          </tr>
-                                          <tr>
-                                            <td valign="top" width="187"><p><strong>Event Mgmt. Secretary</strong></p></td>
-                                            <td valign="top" width="196"><p> Surojit Chatterjee</p></td>
-                                            <td valign="top" width="278"><p> Phone: 678-640-4151&nbsp;<br />
-                                              Email: <strong><a href="mailto:chsurajit@gmail.com">chsurajit@gmail.com</a></strong></p></td>
+                                            <td valign="top" width="196"><p> Prabir Nandi</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:"></a></strong></p></td>
                                           </tr>
                                           <tr>
                                             <td valign="top" width="187"><p> <strong>VP, Publication</strong></p></td>
-                                            <td valign="top" width="196"><p>Arnab Bose</p></td>
-                                            <td valign="top" width="278"><p>Phone: 641-275-2749&nbsp;<br/>
+                                            <td valign="top" width="196"><p>Byasdev Saha</p></td>
+                                            <td valign="top" width="278"><p>Phone: <br/>
                                               Email: <strong><a href="mailto:editor@pujari.org">editor@pujari.org</a></strong></p></td>
                                           </tr>
                                           <tr>
+                                            <td valign="top" width="187"><p><strong>Food &amp; Services Secretary</strong></p></td>
+                                            <td valign="top" width="196"><p> Sanjay Chatterjee</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:"></a></strong></p></td>
+                                          </tr>
+                                          <tr>
                                             <td valign="top" width="187"><p> <strong>Decoration Secretary</strong></p></td>
-                                            <td valign="top" width="196"><p> Paromita Ghosh</p></td>
-                                            <td valign="top" width="278"><p> Phone: 404-964-4927<br />
-                                              Email: <strong><a href="mailto:paromita_ghosh@yahoo.com">paromita_ghosh@yahoo.com</a></strong></p></td>
+                                            <td valign="top" width="196"><p> Tumpa Bhattacharya</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:"></a></strong></p></td>
                                           </tr>
                                           <tr>
                                             <td valign="top" width="187"><p> <strong>Webmaster</strong></p></td>
-                                            <td valign="top" width="196"><p> Rupak Ganguly</p></td>
-                                            <td valign="top" width="278"><p> Phone: 678-648-7434<br />
+                                            <td valign="top" width="196"><p> Saikat Das</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
                                               Email: <strong><a href="mailto:webmaster@pujari.org">webmaster@pujari.org</a></strong></p></td>
                                           </tr>
                                           <tr>
@@ -114,6 +110,17 @@ include("include/header.php");
                                             <td valign="top" width="196"><p> Bulbul Banik</p></td>
                                             <td valign="top" width="278"><p> Phone: 770-579-8594<br />
                                               Email: <strong><a href="mailto:brb67@yahoo.com">brb67@yahoo.com</a></strong></p></td>
+                                          </tr>
+                                          <tr>
+                                            <td rowspan="2" valign="top" width="187"><p><strong>Joint Secretary, Hospitality</strong></p></td>
+                                            <td valign="top" width="196"><p> Rupa Banerjee</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:"></a></strong></p></td>
+                                          </tr>
+                                          <tr>
+                                            <td valign="top" width="196"><p> Madhumita Mukhopadhyay</p></td>
+                                            <td valign="top" width="278"><p> Phone: <br />
+                                              Email: <strong><a href="mailto:"></a></strong></p></td>
                                           </tr>
                                         </tbody>
                                     </table></td>
