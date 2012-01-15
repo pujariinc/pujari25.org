a22f894faa3ef8d18ff93a179b8358be8fc90998
Rupak Ganguly <rupakg@gmail.com>
2012-01-14 21:59:22 -0500

    Rename to archive the NYE page.

---
+++ events_nye2011.php
@@ -0,0 +1,110 @@
+<?php
+include("include/top.php");
+?>
+<meta name="description" content="Pujari is a socio-cultural organization based in Atlanta, Georgia." />
+<meta name="keywords" content="Pujari, Cultural, Organization, Picnic, Cultural Programme, Baisakhi, Lakshmi Puja, Durga Puja, New Year Parties, Bangomela, Anjali, Publication, atlanta, georgia, bengali, durga puja, saraswati puja, laxmi puja, diwali, bengali club, association, cultural, bengali organization, puja, pooja, durga, saraswati, laxmi, lakshmi, rabindra sangeet, picnic, bengali community, community events, baisakhi, poila baishakhi, USA, new year's party, new year's eve, celebration, community participation, natok, dance drama, music kobita, drawing competition, kid's program, bengali adda">
+<title>Pujari | A socio-cultural organization in Atlanta, Georgia</title>
+<?php
+include("include/header.php");
+// ********* Add this part in every page **** //
+?>
+
+<!-- ############# Add page content here ################# -->
+        
+        <div id="content_container">
+        	<table width="100%" cellpadding="0" cellspacing="0" border="0">
+            	<tr>
+                	<!-- Left Sidebar Start Here -->
+                	<td class="content_left">
+                    	<?php include("include/left_sidebar.php"); ?>
+                    </td><!-- Left Sidebar End -->
+                    
+                    <!-- Content Area Start -->
+                    <td class="content_mid">
+                    	<!--<div id="content_hide" class="content_hide">-->
+                        <div>
+                    		<!-- Add homepage content after this -->
+                        	<h1>Next Event: 2012 New Years Eve Party</h1>
+                          <div id="NYEParty" class="event_item clear_both">
+                            <div class="date_bg float_left">
+                              <div class="month">Dec</div>
+                              <div class="date">31</div>
+                            </div>
+                            <div class="sub_content float_left">
+                              <h2>2012 New Years Eve Party</h2>
+                              <h3>Saturday, December 31st, 2011</h3>
+                              <div class="programs">
+                                <h4>Pujari's next event is the most anticipated one, the 2012 New Years Eve Party to be held at Ashiana Banquet Hall (Global Mall).
+                                For more information and details visit: <a href="http://atlantanewyearparty.net" target="_blank">http://atlantanewyearparty.net</a></h4>
+                              </div>
+                            </div>
+                          </div>
+                    		<!-- Add homepage content before this -->
+                        </div>
+						<!--<div class="read_more" id="read_more"><a href="javascript:void(0)" onclick="readmore()">read more &raquo;</a></div>-->
+                        
+                        <!--<div class="sponsors">
+                        	<h2>Sponsors</h2>
+                            
+                            
+                            <div id="slider">
+                                 <div class="marquee" id="mycrawler2"> 
+                                <img src="http://static.flickr.com/66/199481236_dc98b5abb3_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/75/199481072_b4a0d09597_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/57/199481087_33ae73a8de_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/77/199481108_4359e6b971_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/66/199481236_dc98b5abb3_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/75/199481072_b4a0d09597_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/57/199481087_33ae73a8de_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/77/199481108_4359e6b971_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/66/199481236_dc98b5abb3_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/75/199481072_b4a0d09597_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/57/199481087_33ae73a8de_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/77/199481108_4359e6b971_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/66/199481236_dc98b5abb3_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/75/199481072_b4a0d09597_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/57/199481087_33ae73a8de_s.jpg" width="75" height="75" alt="" />
+                                <img src="http://static.flickr.com/77/199481108_4359e6b971_s.jpg" width="75" height="75" alt="" />
+                                </div>
+
+								<script type="text/javascript">
+                                marqueeInit({
+                                    uniqueid: 'mycrawler2',
+                                    style: {
+                                        //'padding': '2px',
+                                        //'width': '690px',
+                                        //'height': '120px',
+                                        //'border': '1px solid #000'
+                                    },
+                                    inc: 20, //speed - pixel increment for each iteration of this marquee's movement
+                                    mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
+                                    moveatleast: 1,
+                                    neutral: 150,
+                                    savedirection: true
+                                });
+                                </script> 
+</div>                    
+                            
+                            
+                        </div>-->
+                    </td><!-- *********** Content Area End *************** -->
+                    
+                    
+                    
+                    
+                	<!-- ############# Right Sidebar Start Here ############## -->
+                    <td class="content_right">
+                    	<?php include("include/right_sidebar.php"); ?>
+                    </td><!-- ############# Right Sidebar End ############## -->
+                    
+                    
+                </tr>
+            </table>
+        </div>
+        
+        
+        
+<?php
+include("include/footer.php");
+// ********* Add this part in every page **** //
+?>
